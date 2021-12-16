af16504a1dde96801b5bfcb00f1dd33cf4ab35ae
Taylor Johnston <tayl1055@purdue.edu>
2021-12-16 01:07:32 -0800

    testing write

--- fp/tftp_client.cpp
+++ fp/tftp_client.cpp
@@ -191,7 +191,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }*/
 
-    return startTransfer("54948", "test.txt", 1);
+    return startTransfer("54948", "test.txt", 2);
 
     //return startTransfer(argv[1], argv[2], stoi(argv[3]), stoi(argv[4]), stoi(argv[5]), stoi(argv[6]));
 }
